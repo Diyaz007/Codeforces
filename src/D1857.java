eced0238379746e041b3a2ec9e7448dad4cbbf60
Diyaz007 <diyaz18.08@gmail.com>
2025-01-30 22:56:41 +0600

    solve 1857D

--- src/D1857.java
+++ src/D1857.java
@@ -6,24 +6,19 @@ public class D1857 {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int tests = sc.nextInt();
-
             while (tests-- > 0) {
-                int n = sc.nextInt();  // Считываем число n
+                int n = sc.nextInt();
                 int[] first = new int[n];
                 int[] second = new int[n];
                 int[] result = new int[n];
-
                 for (int i = 0; i < n; i++) {
                     first[i] = sc.nextInt();
                 }
-
                 for (int i = 0; i < n; i++) {
                     second[i] = sc.nextInt();
                 }
-
                 int maxC = Integer.MIN_VALUE;
                 List<Integer> wise = new ArrayList<>();
-
                 for (int i = 0; i < n; i++) {
                     result[i] = first[i] - second[i];
                     maxC = Math.max(maxC, result[i]);
